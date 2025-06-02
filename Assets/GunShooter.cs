c072a997899ed5f3b9a65b4d4e9d05279f993083
Piero-Jeri <piero.jeri@tecsup.edu.pe>
2025-06-02 12:03:44 -0500

    baneen el curso

--- Assets/GunShooter.cs
+++ Assets/GunShooter.cs
@@ -4,21 +4,20 @@ using UnityEngine.InputSystem;
 public class GunShooter : MonoBehaviour
 {
     [Header("Disparo")]
-    public GameObject bulletPrefab;
+    //public GameObject bulletPrefab;
     public Transform spawnPoint;
     public float bulletSpeed = 20f;
 
-
-    public void OnShoot()
+    private void Start()
     {
-        GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        rb.linearVelocity = spawnPoint.forward * bulletSpeed;
+        Debug.Log("a");
     }
+
     public void OnShoot(InputAction.CallbackContext context)
     {
-        GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        rb.linearVelocity = spawnPoint.forward * bulletSpeed;
+        /*GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
+         Rigidbody rb = bullet.GetComponent<Rigidbody>();
+         rb.linearVelocity = spawnPoint.forward * bulletSpeed;*/
+        Debug.Log("waza trigger");
     }
 }
