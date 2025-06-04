e5b6cdcec2370d2c25063fc567595e72e7b075e4
maradw <maria.mendoza@tecsup.edu.pe>
2025-06-04 17:43:36 -0500

    movement

--- Assets/GunShooter.cs
+++ Assets/GunShooter.cs
@@ -7,12 +7,27 @@ public class GunShooter : MonoBehaviour
     //public GameObject bulletPrefab;
     public Transform spawnPoint;
     public float bulletSpeed = 20f;
-
+    private Vector2 moveInput;
+    public float moveSpeed = 2f;
+    [SerializeField] GameObject test;
+    public Transform vrCamera;
     private void Start()
     {
         Debug.Log("a");
     }
+    private void Update()
+    {
+        /*Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
 
+        // Movemos el objeto en la dirección deseada (opcionalmente relativa al headset/cámara)
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.Self);*/
+        Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y);
+
+
+        Vector3 relativeDirection = vrCamera.TransformDirection(direction);
+    relativeDirection.y = 0; // Evita que se mueva hacia arriba/abajo
+    test.transform.position += relativeDirection * moveSpeed * Time.deltaTime;
+    }
     public void OnShoot(InputAction.CallbackContext context)
     {
         /*GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
@@ -28,7 +43,8 @@ public class GunShooter : MonoBehaviour
     }
     public void OnMove(InputAction.CallbackContext context)
     {
-
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log($"waza joystick: {moveInput}");
         Debug.Log("waza joystick");
     }
     public void OnPrimary(InputAction.CallbackContext context)
